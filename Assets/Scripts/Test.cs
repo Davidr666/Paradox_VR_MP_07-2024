bea81c49dd6804e755aef56706f3ca7d09c14971
CarlosSolis26 <kxr10s1@gmail.com>
2024-08-07 22:44:42 -0500

    7/8/2024

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    //delegate void Operacion(int a, int b);
+    //// Start is called before the first frame update
+
+    //// Modif acceso, tipo ret, nombre
+    //private void Sumar()
+    //{
+    //    Debug.Log("El valor de la suma es: " + 4);
+    //}
+    //private void Restar(int a, int b)
+    //{
+    //    var n = a - b;
+    //    Debug.Log("El valor de la resta es: " + n);
+    //}
+    //private void Multiplicar()
+    //{
+    //    Debug.Log("El valor de la multiplicación es: " + 2);
+    //}
+    //private void Dividir(int a, int b)
+    //{
+    //    var resultado = a / b;
+    //    Debug.Log("El valor de la división es: " + resultado);
+    //}
+
+    //void Start()
+    //{
+    //    Operacion operacion = new Operacion(Dividir);
+
+    //    operacion += Restar(4,3);
+    //    operacion += Multiplicar;
+    //    operacion += Dividir;
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
